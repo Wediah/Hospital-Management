f05959d5d87d3aabc3fcc5061fcd9d2338a76f33
Wediah <100675006+Wediah@users.noreply.github.com>
2023-08-01 23:58:01 +0000

    slight changes to main page

--- index.php
+++ index.php
@@ -30,7 +30,7 @@
                     <h3>Patients</h3>
                 </div>
                 <div class="button-container">
-                    <button >Log in</button>
+                    <button data-bs-toggle="modal" data-bs-target="#exampleModal">Log in</button>
                 </div>
             </div>
             <div class="log-items">
@@ -101,6 +101,33 @@
             </div>
             
 
+        </div>
+    </div>
+
+
+    <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered">
+            <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">Log in</h5>
+                <button type="button" class="btn border-0" data-bs-dismiss="modal" aria-label="Close"><span class="material-symbols-outlined">
+                close
+                </span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <form class="register-form">
+                    <input type="text" placeholder="email address"/>
+                    <input type="password" placeholder="password"/>
+                    <button class="ins">Sign in 
+                    </button>  
+                    <div class="message">
+                    <div>Not registered?</div>
+                    <div class="ab" data-bs-toggle="modal" data-bs-target="#signinModal">sign up</div>
+                    </div>
+                </form>
+            </div>
+            </div>
         </div>
     </div>
 
