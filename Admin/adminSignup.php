a8dd608e946188eeb1813f40efa92b96cbbec92a
Wediah <100675006+Wediah@users.noreply.github.com>
2023-08-02 17:45:28 +0000

    moved the connectivity to proccess file

--- Admin/adminSignup.php
+++ Admin/adminSignup.php
@@ -1,29 +1,4 @@
-<?php
-    $host = "localhost:3306";
-    $user = "root";
-    $pass = "";
-    $dbname = "hospitalmanagement";
 
-    $conn = mysqli_connect($host, $user, $pass, $dbname);
-
-    if (!$conn){
-        die('Could not connect: '.mysqli_connect_error());
-    }
-    echo 'Connected successfully <br/>';
-
-    if ($_SERVER["REQUEST_METHOD"] == "POST") {
-        $fname = $_POST["fname"];
-        $email = $_POST["email"];
-        $phone = $_POST["phone"];
-        $password = $_POST["password"];
-
-        $sql = "INSERT INTO admin (fullname, email, phone, password) VALUES ('$fname', '$email', '$phone', '$password')";
-
-        if (mysqli_query($conn,$sql)){
-            echo 'Sign up successfully';
-        } 
-    }
-?>
 
 
 
@@ -42,7 +17,7 @@
         <h3>Create a new account</h3>
         <p>It's quick and simple</p>
         <hr>
-        <form name="signup" action="" method="post" onsubmit="return submitForm()">
+        <form name="signup" action="processSignup.php" method="post" onsubmit="return submitForm()">
         <input type="text" name="fname" id="fname" placeholder="Full Name">
         <input type="text" name="email" id="email" placeholder="Email Address">
         <input type="text" name="phone" id="phone" placeholder="Phone Number">
