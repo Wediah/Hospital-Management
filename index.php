6a31f55497474a4791e5a9553ac28eb28e08bdc2
Wediah <100675006+Wediah@users.noreply.github.com>
2023-08-03 22:58:28 +0000

    removes extra items containers

--- index.php
+++ index.php
@@ -14,10 +14,7 @@
         <h1>
             Welcome to Lucy Mememorial!
         </h1>
-        <p>Kindly log into respective module to access information.</p>
-        <button class="scroll">
-            scroll down for more
-        </button>
+        <p>Kindly log into the respective module to access information.</p>
     </div>
     
     <div class="Centered-container">
@@ -104,87 +101,6 @@
         </div>
     </div>
 
-    <div class="logs-container2">
-            <div class="log-items">
-                <div>
-                    <span  class="material-symbols-outlined">
-                        person
-                    </span>
-                    <h3>Patients</h3>
-                </div>
-                <div class="button-container">
-                    <a href="">Log in</a>
-                </div>
-            </div>
-            <div class="log-items">
-                <div>
-                    <span  class="material-symbols-outlined">
-                        stethoscope
-                    </span>
-                    <h3>Doctor</h3>
-                </div>
-                <div class="button-container">
-                    <a href="">Log in</a>
-                </div>
-            </div>
-            <div class="log-items">
-                <div>
-                    <span  class="material-symbols-outlined">
-                        clinical_notes
-                    </span>
-                    <h3>Nurse</h3>
-                </div>
-                <div class="button-container">
-                    <a href="">Log in</a>
-                </div>
-            </div>
-            <div class="log-items">
-                <div>
-                    <span  class="material-symbols-outlined">
-                        medication
-                    </span>
-                    <h3>Pharmacist</h3>
-                </div>
-                <div class="button-container">
-                    <a href="">Log in</a>
-                </div>
-            </div>
-            <div class="log-items">
-                <div>
-                    <span  class="material-symbols-outlined">
-                        biotech
-                    </span>
-                    <h3>Laboratorist</h3>
-                </div>
-                <div class="button-container">
-                    <a href="">Log in</a>
-                </div>
-            </div>
-            <div class="log-items">
-                <div>
-                    <span  class="material-symbols-outlined">
-                        account_balance
-                    </span>
-                    <h3>Accountant</h3>
-                </div>
-                <div class="button-container">
-                    <a href="">Log in</a>
-                </div>
-            </div>
-            <div class="log-items">
-                <div>
-                    <span  class="material-symbols-outlined">
-                        shield_person
-                    </span>
-                    <h3>Admin</h3>
-                </div>
-                <div id="loginAdmin" class="button-container">
-                    <a href="">Log in</a>
-                </div>
-            </div>
-            
-
-        </div>
 </body>
     
     
