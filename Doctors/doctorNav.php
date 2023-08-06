dfffdee07d06ce62a9dba795d6435684c900b9f9
Wediah <100675006+Wediah@users.noreply.github.com>
2023-08-06 23:29:10 +0000

    adds a side nav

---
+++ Doctors/doctorNav.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Doctor</title>
+    <link rel="stylesheet" href="doctorNav.css">
+</head>
+<body>
+<div id="mySidenav" class="sidenav">
+    <div class="image-class">
+        <img src="" alt="">
+        <h4>welcome</h4>
+    </div>
+    <div class="links-only">
+        <a href="#">Appointments</a>
+        <a href="#">Priscriptions</a>
+        <a href="#">Medications</a>
+        <a href="#">Laboratory</a>
+        <a href="">Surgery</a>
+    </div>
+
+    <div class="log--out">
+        <a href="">Profile</a>
+        <a href="">logout</a>
+    </div>
+</div>
+</body>
+</html>
