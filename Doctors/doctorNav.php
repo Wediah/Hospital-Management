a8ac722499a06f12b07f80145f26a8be395da630
Wediah <100675006+Wediah@users.noreply.github.com>
2023-08-07 01:08:11 +0000

    adds navbar

--- Doctors/doctorNav.php
+++ Doctors/doctorNav.php
@@ -5,24 +5,64 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Doctor</title>
     <link rel="stylesheet" href="doctorNav.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0" />
 </head>
 <body>
 <div id="mySidenav" class="sidenav">
     <div class="image-class">
-        <img src="" alt="">
-        <h4>welcome</h4>
+        <img src="/images/hero1.jpg" alt="">
+        <h3>Welcome, <span>Emmanuel Wediah</span></h3>
     </div>
     <div class="links-only">
-        <a href="#">Appointments</a>
-        <a href="#">Priscriptions</a>
-        <a href="#">Medications</a>
-        <a href="#">Laboratory</a>
-        <a href="">Surgery</a>
+        
+            
+        <a href="#">
+            <span class="material-symbols-outlined">
+            assignment_add
+            </span>
+            <h4>Appointments</h4>
+        </a>
+        
+        <a href="#">
+            <span class="material-symbols-outlined">
+            prescriptions
+            </span>
+            <h4>Prescriptions</h4>
+        </a>
+        <a href="#">
+            <span class="material-symbols-outlined">
+            medication
+            </span>
+            <h4>Medications</h4>
+        </a>
+        <a href="#">
+            <span class="material-symbols-outlined">
+            biotech
+            </span>
+            <h4>Laboratory</h4>
+        </a>
+        <a href="">
+            <span class="material-symbols-outlined">
+            surgical
+            </span>
+            <h4>Surgery</h4>
+        </a>
     </div>
 
     <div class="log--out">
-        <a href="">Profile</a>
-        <a href="">logout</a>
+        <a href="">
+            <span class="material-symbols-outlined">
+            person
+            </span>
+            <h4>Profile</h4>
+        </a>
+        <a href="./logout3.php">
+            
+            <span class="material-symbols-outlined">
+            logout
+            </span>
+            <h4>logout</h4>
+        </a>
     </div>
 </div>
 </body>
